2e74de31fc0021cc8d5a59e8def7862b8f1e6aa3
Veasna Youphon <veasna.youphon2@boeing.com>
2018-11-15 15:24:12 -0800

    Added Prod code

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -7,8 +7,10 @@ const json = JSON.stringify(jsf(schema));
 
 fs.writeFile("./src/api/db.json", json, function (err) {
   if (err) {
+    // eslint-disable-next-line no-console
     return console.log(chalk.red(err));
   } else {
+    // eslint-disable-next-line no-console
     console.log(chalk.green("Mock data generated."));
   }
 });
